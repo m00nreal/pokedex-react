187308d6ece6bbcb366e8ed96c0bf2d0603cc8c3
Jair Luna <arel.jair@gmail.com>
2020-08-16 00:03:21 -0500

    fixed for deploy
    (cherry picked from commit d8e6804ae3579bf5600d65ce5e3b2cc0bf115f89)

---
+++ docs/precache-manifest.acfa9ff059b93ff085cd3fd491e7a9de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc8386cece9caae08c2e5ff795d9252b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3536c5b627d7c177e903",
+    "url": "/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "3f8a18d4fe9deed06871",
+    "url": "/static/css/main.79d62207.chunk.css"
+  },
+  {
+    "revision": "3536c5b627d7c177e903",
+    "url": "/static/js/2.6420bb58.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.6420bb58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f8a18d4fe9deed06871",
+    "url": "/static/js/main.5eafae91.chunk.js"
+  },
+  {
+    "revision": "92e2dfe38426db94cc6d",
+    "url": "/static/js/runtime-main.536afbf1.js"
+  }
+]);
